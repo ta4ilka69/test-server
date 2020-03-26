ff7c300269d4de7f901ef7d18abeed115bc6f47a
ta4ilka69 <51398469+ta4ilka69@users.noreply.github.com>
2020-03-26 12:43:40 +0300

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,41 +1,43 @@
+/* eslint-disable linebreak-style */
 class Storage {
   constructor() {
     this.items = [];
     this.id = 1;
   }
+
   add(s) {
     this.items.unshift({ value: s, id: this.id });
     this.id += 1;
   }
 
   edit(value, ind) {
-    const i = this.items.findIndex(el => el.id === ind);
+    const i = this.items.findIndex((el) => el.id === ind);
     this.items[i].value = value;
   }
 
   delete(id) {
-    const i = this.items.findIndex(el => el.id === id);
+    const i = this.items.findIndex((el) => el.id === id);
     this.items.splice(i, 1);
   }
 
   list() {
-    let temps = "<ul>";
-    for (let i = 0; i < this.items.length; i++) {
-      let id = this.items[i].id;
-      temps +=
-          `<li>` +
-          this.items[i].value +
-        `  ` +
-        `<a href="/edit?id=${id}">Edit</a>` +
-        `  ` +
-        `<a href="/delete?id=${id}">Delete</a>` +
-        `</li>`;
+    let temps = '<ul>';
+    for (let i = 0; i < this.items.length; i += 1) {
+      const { id } = this.items[i];
+      temps
+        += `<li>${this.items[i].value}  `
+        + `<a href="/edit?id=${id}">Edit</a>`
+        + '  '
+        + `<a href="/delete?id=${id}">Delete</a>`
+        + '</li>';
     }
-    temps += "</ul>";
+    temps += '</ul>';
     return temps;
-    }
-    hasid(id) {
-        return this.items.findIndex((item) => item.id === id) >= 0;
+  }
+
+
+  hasid(id) {
+    return this.items.findIndex((item) => item.id === id) >= 0;
   }
 }
 
