24074cd1ffcacd6b9f5e78e3a4514feaa1fb42d2
ta4ilka69 <51398469+ta4ilka69@users.noreply.github.com>
2020-03-26 12:43:44 +0300

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -1,27 +1,26 @@
 const express = require('express');
-const storage = require('./Storage.js');
+const Storage = require('./Storage.js');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-const store = new storage();
+const store = new Storage();
 
-
-app.get('/', function (req, res) {
-  let template = store.list();
+app.get('/', (req, res) => {
+  const template = store.list();
   res.send(
-    template +
-      `
+    `${template}
         <form action="/add" method="post">
             <input type="text" placeholder="Data" name="data"/>
             <button type="submit">Send</button>
 </form>
-`
+`,
   );
 });
 
-app.get('/delete', function (req, res) {
-  let tmpid = parseInt(req.query.id, 10);
+app.get('/delete', (req, res) => {
+  const tmpid = parseInt(req.query.id, 10);
   if (store.hasid(tmpid)) {
     store.delete(tmpid);
     res.redirect('/');
@@ -30,32 +29,28 @@ app.get('/delete', function (req, res) {
   }
 });
 
-app.get('/edit', function (req, res) {
-  let id = parseInt(req.query.id,10);
+app.get('/edit', (req, res) => {
+  const id = parseInt(req.query.id, 10);
   if (store.hasid(id)) {
     res.send(`<form action="/edit?id=${id}" method="post">
             <input type="text" placeholder="Editor" name="eddata"/>
             <button type="submit">Edit</button> </form>`);
   } else {
     res.status(404).send('Not found');
-    return;
   }
 });
 
-
-app.post('/edit', function (req, res) {
-  let tmpid = parseInt(req.query.id, 10);
+app.post('/edit', (req, res) => {
+  const tmpid = parseInt(req.query.id, 10);
   if (store.hasid(tmpid)) {
     store.edit(req.body.eddata, tmpid);
     res.redirect('/');
   } else {
     res.status(404).send('Not found');
-    return;
   }
 });
 
-
-app.post('/add', function (req, res) {
+app.post('/add', (req, res) => {
   store.add(req.body.data);
   res.redirect('/');
 });
