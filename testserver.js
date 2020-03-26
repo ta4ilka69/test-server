ab0a72229391ded3334d39fd5a477870d957e81d
ta4ilka69 <51398469+ta4ilka69@users.noreply.github.com>
2020-03-26 10:48:02 +0300

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -1,53 +1,63 @@
-// JavaScript source code
-const express = require("express");
-
+const express = require('express');
+const storage = require('./Storage.js');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-const stack = [];
-let counter = 0;
+const store = new storage();
 
-app.get("/", function (req, res) {
-    let template = "<ul>";
-    for (let i = 0; i < stack.length; i++) {
-        let id = stack[i].id;
-        template += (`<li>` + stack[i].value + `  ` + `<a href="/edit?id=${id}">Edit</a>` + `  ` + `<a href="/delete?id=${id}">Delete</a>` + `</li>`);
-    }
-    template += "</ul>";
-    res.send(template+`
+
+app.get('/', function (req, res) {
+  let template = store.list();
+  res.send(
+    template +
+      `
         <form action="/add" method="post">
             <input type="text" placeholder="Data" name="data"/>
             <button type="submit">Send</button>
 </form>
-`)});
-app.get("/delete", function (req, res)
-{
-    let tmpid = parseInt(req.query.id);
-    const i = stack.findIndex((el) => el.id === tmpid)
-    stack.splice(i, 1);
-    res.redirect("/");
-})
+`
+  );
+});
 
-app.get("/edit", function (req, res) {
-    let id = parseInt( req.query.id);
+app.get('/delete', function (req, res) {
+  let tmpid = parseInt(req.query.id, 10);
+  if (store.hasid(tmpid)) {
+    store.delete(tmpid);
+    res.redirect('/');
+  } else {
+    res.status(404).send('Not found');
+  }
+});
+
+app.get('/edit', function (req, res) {
+  let id = parseInt(req.query.id);
+  if (store.hasid(id)) {
     res.send(`<form action="/edit?id=${id}" method="post">
             <input type="text" placeholder="Editor" name="eddata"/>
-            <button type="submit">Edit</button> </form>`)
-})
-
-app.post("/edit", function (req, res) {
-    let tmpid = parseInt(req.query.id);
-    const i = stack.findIndex((el) => el.id === tmpid)
-    stack[i].value = req.body.eddata;
-    res.redirect("/");
-})
+            <button type="submit">Edit</button> </form>`);
+  } else {
+    res.status(404).send('Not found');
+    return;
+  }
+});
 
 
-app.post("/add", function (req, res) {
-    counter++;
-    stack.unshift({ value : req.body.data, id : counter });
-    res.redirect("/");
+app.post('/edit', function (req, res) {
+  let tmpid = parseInt(req.query.id, 10);
+  if (store.hasid(tmpid)) {
+    store.edit(req.body.eddata, tmpid);
+    res.redirect('/');
+  } else {
+    res.status(404).send('Not found');
+    return;
+  }
+});
+
+
+app.post('/add', function (req, res) {
+  store.add(req.body.data);
+  res.redirect('/');
 });
 
 app.listen(3000);
